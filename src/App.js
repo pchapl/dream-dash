9239e645a47a84c8e6d4cfb0561edf9599981f7b
Pavel Chaplygin <mail@pchapl.dev>
2023-02-04 21:41:42 +0200

    Cleanup; Add google charts; Add MyPie component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
+import MyPie from "./components/MyPie";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const inc = () => setCounter(counter + 1);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          {counter}
-        </p>
-        <p className="App-link" onClick={inc}>
-          btn
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <section>
+                <MyPie/>
+            </section>
+        </div>
+    );
 }
 
 export default App;
